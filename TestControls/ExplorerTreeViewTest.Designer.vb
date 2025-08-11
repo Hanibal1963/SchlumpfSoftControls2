394e46de1db91f859715f615d06fc503466c588a
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-08-11 20:39:40 +0200

    Neue Steuerelemente und Funktionalität im ExplorerTreeViewTest
    In der Datei `ExplorerTreeViewTest.Designer.vb` wurden die Steuerelemente
    `Button1`, `TextBox1` und `FolderBrowserDialog1` hinzugefügt. Zudem wurden
    die Position und der Stil von `Label1` geändert, und die Größe des
    Formulars angepasst.
    
    In der Datei `ExplorerTreeViewTest.resx` wurde eine neue Metadatenzeile
    für `FolderBrowserDialog1.TrayLocation` hinzugefügt, die die Position
    des Dialogs definiert.
    
    In der Datei `ExplorerTreeViewTest.vb` wurde die Methode
    `Button1_Click` implementiert, um das Auswählen eines Ordners zu
    ermöglichen und den Pfad im `ExplorerTreeView1` zu erweitern.
    Zusätzlich wurde die Methode `ExplorerTreeView1_SelectedPathChanged`
    aktualisiert, um die Lesbarkeit zu verbessern.

--- TestControls/ExplorerTreeViewTest.Designer.vb
+++ TestControls/ExplorerTreeViewTest.Designer.vb
@@ -24,6 +24,9 @@ Partial Class ExplorerTreeViewTest
     Private Sub InitializeComponent()
         Me.ExplorerTreeView1 = New SchlumpfSoft.Controls.ExplorerTreeViewControl.ExplorerTreeView()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
         Me.SuspendLayout()
         '
         'ExplorerTreeView1
@@ -46,24 +49,55 @@ Partial Class ExplorerTreeViewTest
         'Label1
         '
         Me.Label1.AutoEllipsis = True
-        Me.Label1.Location = New System.Drawing.Point(16, 442)
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(16, 468)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(538, 57)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Label1"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(527, 434)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(27, 22)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(19, 434)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(502, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.WordWrap = False
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "suche Pfad"
+        Me.FolderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer
+        '
         'ExplorerTreeViewTest
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ExplorerTreeView1)
         Me.Name = "ExplorerTreeViewTest"
-        Me.Size = New System.Drawing.Size(576, 516)
+        Me.Size = New System.Drawing.Size(566, 542)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ExplorerTreeView1 As SchlumpfSoft.Controls.ExplorerTreeViewControl.ExplorerTreeView
     Friend WithEvents Label1 As Label
+    Private WithEvents Button1 As Button
+    Private WithEvents TextBox1 As TextBox
+    Private WithEvents FolderBrowserDialog1 As FolderBrowserDialog
 End Class
--- TestControls/ExplorerTreeViewTest.vb
+++ TestControls/ExplorerTreeViewTest.vb
@@ -2,10 +2,21 @@
 
 Public Class ExplorerTreeViewTest
 
-    Private Sub ExplorerTreeView1_SelectedPathChanged(sender As Object, e As EventArgs) Handles ExplorerTreeView1.SelectedPathChanged
+    Private Sub ExplorerTreeView1_SelectedPathChanged(sender As Object, e As EventArgs) Handles _
+        ExplorerTreeView1.SelectedPathChanged
 
         Dim selpath As String = CType(sender, SchlumpfSoft.Controls.ExplorerTreeViewControl.ExplorerTreeView).SelectedPath
         Label1.Text = $"aktuell ausgewählter Pfad:{vbCrLf}{selpath}"
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles _
+        Button1.Click
+
+        Dim result As DialogResult = FolderBrowserDialog1.ShowDialog
+        If result = DialogResult.OK Then
+            ExplorerTreeView1.ExpandPath(FolderBrowserDialog1.SelectedPath)
+        End If
 
     End Sub
 
