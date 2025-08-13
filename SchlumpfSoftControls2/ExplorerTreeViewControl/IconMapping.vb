b2a50283ba23cbf858c8ada4b990536cf5c0721c
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-08-13 20:06:18 +0200

    Neue Klasse IconMapping zur Verwaltung von Bildschlüsseln
    Die Änderungen in der Datei `IconMapping.vb` fügen eine neue Klasse `IconMapping` hinzu, die die Zuordnung von Namen (wie Ordner- und Laufwerksnamen) zu Bildschlüsseln verwaltet. Ein Dictionary wird erstellt, das verschiedene Zuordnungen von Namen zu Bildschlüsseln enthält.
    
    Zusätzlich wird eine öffentliche Funktion `GetImageKey` bereitgestellt, die den entsprechenden Bildschlüssel für einen gegebenen Namen zurückgibt oder eine leere Zeichenfolge zurückgibt, wenn der Name nicht gefunden wird. Die hinzugefügten Kommentare sind in deutscher Sprache und beschreiben die Funktionalität der Klasse und ihrer Methoden.

---
+++ SchlumpfSoftControls2/ExplorerTreeViewControl/IconMapping.vb
@@ -0,0 +1,65 @@
+﻿' *************************************************************************************************
+' 
+' IconMapping.vb
+' Copyright (c) 2025 by Andreas Sauer 
+'
+' Kurzbeschreibung:
+' 
+' Klasse zum verwalten der Ordner - und Laufwerkssymbole
+' 
+' *************************************************************************************************
+
+Imports System.Collections.Generic
+
+Namespace ExplorerTreeViewControl
+
+    ''' <summary>
+    ''' Verwaltet die Zuordnung von Namen (z. B. Ordnernamen oder Laufwerksnamen) zu ImageKeys.
+    ''' </summary>
+    Friend Class IconMapping
+
+        ''' <summary>
+        ''' Dictionary mit Zuordnungen von Ordnernamen oder Laufwerkstypen zu ImageKeys
+        ''' </summary>
+        ''' <remarks></remarks>
+        Private Shared ReadOnly mappings As New Dictionary(Of String, String) From {
+            {"Computer", "Computer"},
+            {"Desktop", "FolderDesktop"},
+            {"Dokumente", "FolderDocuments"},
+            {"Downloads", "FolderDownloads"},
+            {"Musik", "FolderMusic"},
+            {"Bilder", "FolderPictures"},
+            {"Videos", "FolderVideos"},
+            {"Folder", "Folder"},
+            {"System", "DriveSystem"},
+            {"Fixed", "DriveFixed"},
+            {"CDROM", "DriveCDROM"},
+            {"Floppy", "DriveFloppy"},
+            {"Removable", "DriveRemovable"},
+            {"Network", "DriveNetwork"},
+            {"RamDisk", "DiveRamDisk"},
+            {"NoRoot", "DriveNoRoot"},
+            {"Unknown", "DriveUnknown"}
+        }
+
+        ''' <summary>
+        ''' Gibt den ImageKey für den angegebenen Namen zurück.
+        ''' </summary>
+        ''' <param name="IconTypeString">
+        ''' Der Name (z. B. Ordner- oder Laufwerksname).
+        ''' </param>
+        ''' <returns>
+        ''' Der zugehörige ImageKey oder eine leere Zeichenfolge.
+        ''' </returns>
+        Public Shared Function GetImageKey(IconTypeString As String) As String
+            If mappings.ContainsKey(IconTypeString) Then
+                Return mappings(IconTypeString)
+            Else
+                Return String.Empty ' Standardwert für unbekannte Schlüssel
+            End If
+        End Function
+
+    End Class
+
+
+End Namespace
