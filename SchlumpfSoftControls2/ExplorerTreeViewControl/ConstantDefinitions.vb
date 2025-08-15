808b13b0fb3078b4e29220390497cda06772bcbc
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-08-15 19:15:33 +0200

    Füge neue Symbole für Ordner und Laufwerke hinzu
    In der Datei `ConstantDefinitions.vb` wurden mehrere Konstanten für verschiedene
    Ordner- und Laufwerkssymbole hinzugefügt. Die neuen Konstanten umfassen
    `ICON_FOLDER_FOLDER`, `ICON_DRIVE_SYSTEM`, `ICON_DRIVE_FIXED`,
    `ICON_DRIVE_CDROM`, `ICON_DRIVE_FLOPPY`, `ICON_DRIVE_REMOVABLE`,
    `ICON_DRIVE_NETWORK`, `ICON_DRIVE_RAM`, `ICON_DRIVE_NOROOT` und
    `ICON_DRIVE_UNKNOWN`. Diese Änderungen erweitern die Symboldefinitionen,
    die in der Anwendung verwendet werden können.

--- SchlumpfSoftControls2/ExplorerTreeViewControl/ConstantDefinitions.vb
+++ SchlumpfSoftControls2/ExplorerTreeViewControl/ConstantDefinitions.vb
@@ -45,6 +45,15 @@ Namespace ExplorerTreeViewControl
         Public Const ICON_FOLDER_PICTURES As String = "FolderPictures"
         Public Const ICON_FOLDER_VIDEOS As String = "FolderVideos"
         Public Const ICON_FOLDER_FOLDER As String = "Folder"
+        Public Const ICON_DRIVE_SYSTEM As String = "DriveSystem"
+        Public Const ICON_DRIVE_FIXED As String = "DriveFixed"
+        Public Const ICON_DRIVE_CDROM As String = "DriveCDROM"
+        Public Const ICON_DRIVE_FLOPPY As String = "DriveFloppy"
+        Public Const ICON_DRIVE_REMOVABLE As String = "DriveRemovable"
+        Public Const ICON_DRIVE_NETWORK As String = "DriveNetwork"
+        Public Const ICON_DRIVE_RAM As String = "DiveRamDisk"
+        Public Const ICON_DRIVE_NOROOT As String = "DriveNoRoot"
+        Public Const ICON_DRIVE_UNKNOWN As String = "DriveUnknown"
 
 #End Region
 
