dbf5e5a841032383749e5c8b5ab519fa22e4b5e1
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-08-17 12:39:14 +0200

    Kommentare in DriveNode aktualisiert und verbessert
    Die Kommentare in der Klasse `DriveNode` wurden überarbeitet, um die Verwendung von
    `<see cref="..."/>` für Verweise auf andere Klassen und Eigenschaften zu optimieren.
    Der Kommentar für die `DriveType`-Eigenschaft wurde angepasst, um die
    `DriveInfo`-Klasse korrekt zu referenzieren. Zudem wurde ein neuer Kommentar für
    den Konstruktor hinzugefügt, der die Parameter und die Funktionalität der Klasse
    beschreibt. Die vorherigen mehrzeiligen Kommentare wurden in eine einzige Zeile
    zusammengefasst, um die Lesbarkeit zu erhöhen.

--- SchlumpfSoftControls2/ExplorerTreeViewControl/DriveNode.vb
+++ SchlumpfSoftControls2/ExplorerTreeViewControl/DriveNode.vb
@@ -19,9 +19,7 @@ Namespace ExplorerTreeViewControl
         ''' <summary>
         ''' Gibt den vollständigen Pfad des Knotens zurück
         ''' </summary>
-        ''' <remarks>
-        ''' Diese Eigenschaft gibt den Pfad des Laufwerks zurück, das im Tag gespeichert ist.
-        ''' </remarks>
+        ''' <remarks>Diese Eigenschaft gibt den Pfad des Laufwerks zurück, das im Tag gespeichert ist.</remarks>
         Public Overloads ReadOnly Property FullPath As String
             Get
                 Return Tag.ToString()
@@ -31,15 +29,18 @@ Namespace ExplorerTreeViewControl
         ''' <summary>
         ''' Gibt den Laufwerkstyp des Knotens zurück
         ''' </summary>
-        ''' <remarks>
-        ''' Diese Eigenschaft verwendet die DriveInfo-Klasse, um den Typ des Laufwerks zu ermitteln.
-        ''' </remarks>
+        ''' <remarks>Diese Eigenschaft verwendet <see cref="DriveInfo"/>, um den Typ des Laufwerks zu ermitteln.</remarks>
         Public ReadOnly Property DriveType As DriveType
             Get
                 Return New DriveInfo(Tag.ToString()).DriveType
             End Get
         End Property
 
+        ''' <summary>
+        ''' Initialisiert eine neue Instanz von <see cref="SchlumpfSoft.Controls.ExplorerTreeViewControl.DriveNode"/>. 
+        ''' </summary>
+        ''' <param name="Drive">Laufwerk für welches diese Instanz erstellt werden soll als <see cref="DriveInfo"/></param>
+        ''' <remarks></remarks>
         <Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub New(Drive As DriveInfo)
 
