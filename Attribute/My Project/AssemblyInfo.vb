17b51daa096c503c79fa68fac9db63ff33ef478f
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-09-06 17:52:05 +0200

    Neues Projekt "Attribute" erstellt und konfiguriert
    Die Änderungen umfassen die vollständige Erstellung und Konfiguration eines neuen
    Projekts namens "Attribute". Es wurden mehrere Dateien hinzugefügt, darunter die
    Projektdatei `Attribute.vbproj`, die grundlegende Eigenschaften und Einstellungen
    des Projekts definiert, einschließlich Ziel-Framework-Version und Assembly-Informationen.
    
    Zusätzlich wurden Auto-Generated-Kommentare in `Application.Designer.vb` und
    `Resources.Designer.vb` hinzugefügt, um darauf hinzuweisen, dass Änderungen an
    diesen Dateien verloren gehen können, wenn der Code erneut generiert wird.
    
    Die Datei `Application.myapp` definiert Anwendungsdaten wie
    `EnableVisualStyles` und `SaveMySettingsOnExit`. In `AssemblyInfo.vb` wurden
    allgemeine Informationen zur Assembly hinzugefügt, während die `Resources.resx`-Datei
    Ressourcen im XML-Format speichert.
    
    Einstellungen für die Anwendung wurden in `Settings.Designer.vb` und
    `Settings.settings` definiert, einschließlich der automatischen Speicherung von
    Einstellungen beim Beenden der Anwendung.
    
    Die Datei `ProvideToolboxControlAttribute.vb` wurde hinzugefügt, um ein Attribut
    zu definieren, das der Assembly einen Schlüssel für den Toolbox Controls Installer
    hinzufügt.
    
    Abschließend wurden Änderungen an den Projektdateien vorgenommen, um das neue
    "Attribute"-Projekt in die Lösung `SchlumpfSoftControls2.sln` einzufügen,
    einschließlich der Konfiguration für Debug- und Release-Builds.

---
+++ Attribute/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("SchlumpfSoftAttribute")>
+<Assembly: AssemblyDescription("Bibliothek mit Benutzerdefinierten Attributen")>
+<Assembly: AssemblyCompany("SchlumpfSoft")>
+<Assembly: AssemblyProduct("SchlumpfSoft Controls")>
+<Assembly: AssemblyCopyright("Copyright © Andreas Sauer 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("a53cbc2b-0bbc-4605-96ef-a1c3513abf15")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.2024.12.23")>
+<Assembly: AssemblyFileVersion("1.2024.12.23")>
+<Assembly: NeutralResourcesLanguage("de-DE")>
