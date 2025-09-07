d9013f21426661522c22330b56506a0ce07db6ad
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-09-07 18:58:33 +0200

    Fehlerbereinigung

--- DriveWatcherConrol/My Project/Settings.Designer.vb
+++ DriveWatcherConrol/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.SchlumpfSoft.Conrols.My.MySettings
+        Friend ReadOnly Property Settings() As Global.SchlumpfSoft.Controls.My.MySettings
             Get
-                Return Global.SchlumpfSoft.Conrols.My.MySettings.Default
+                Return Global.SchlumpfSoft.Controls.My.MySettings.Default
             End Get
         End Property
     End Module
