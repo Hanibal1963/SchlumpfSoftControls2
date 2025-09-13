95d76907f7d71a98d9ffc2f42003af65890be148
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-09-13 12:06:32 +0200

    Fehlerbehebung

---
+++ SevenSegmentControl/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("SevenSegmentControl")>
+<Assembly: AssemblyDescription("Zwei Controls zum Anzeigen von Zeichen als 7-Segmentanzeige.")>
+<Assembly: AssemblyCompany("Andreas Sauer")>
+<Assembly: AssemblyProduct("SevenSegmentControl")>
+<Assembly: AssemblyCopyright("Copyright © Andreas Sauer 2025")>
+<Assembly: AssemblyTrademark("privat")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("e8704c85-423c-4a02-850b-da97e0f0b782")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+
+<Assembly: AssemblyVersion("2.2025.0907.0")>
+<Assembly: AssemblyFileVersion("2.2025.0907.0")>
