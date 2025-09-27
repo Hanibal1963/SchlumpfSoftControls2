e3e331654f4efd7d4af7b7bca4cad9c851d25bc5
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-09-27 11:44:12 +0200

    Aktualisiere AssemblyInfo.vb mit neuen Versionsnummern
    Aktualisiere die Versionsnummern der Assembly von "1.2025.0914.0" auf
    "1.2025.0927.0" für sowohl `AssemblyVersion` als auch
    `AssemblyFileVersion`.

--- SchlumpfSoftControlsDemo/My Project/AssemblyInfo.vb
+++ SchlumpfSoftControlsDemo/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("1.2025.0914.0")>
-<Assembly: AssemblyFileVersion("1.2025.0914.0")>
+<Assembly: AssemblyVersion("1.2025.0927.0")>
+<Assembly: AssemblyFileVersion("1.2025.0927.0")>
