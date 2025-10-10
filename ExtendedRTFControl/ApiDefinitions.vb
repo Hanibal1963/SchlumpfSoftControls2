0953ff583c060725b28a07e989ba7d0aa7680208
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-10-10 20:23:07 +0200

    Control "Erweiterte RTF Textbox" hinzugefügt.

---
+++ ExtendedRTFControl/ApiDefinitions.vb
@@ -0,0 +1,13 @@
+﻿
+Namespace ExtendedRTFControl
+
+    Module ApiDefinitions
+
+        ' Sendet ein Windows-Message direkt an ein Fenster (hier: WM_SETREDRAW zum Ein/Aus-Schalten der Darstellung).
+        <System.Runtime.InteropServices.DllImport("user32.dll")>
+        Friend Function SendMessage(hWnd As System.IntPtr, msg As Integer, wParam As Boolean, lParam As System.IntPtr) As System.IntPtr
+        End Function
+
+    End Module
+
+End Namespace
