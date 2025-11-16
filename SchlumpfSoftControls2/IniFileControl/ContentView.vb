b713ec6bd961545087960127bb1572ed5fbb92bc
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-16 16:29:37 +0100

    Refactor: Verbesserte Struktur und Konsistenz
    - Attribute durch vollqualifizierte ersetzt, um Konflikte zu vermeiden.
    - Variablendeklarationen in `#Region "Variablendefinition"` verschoben.
    - Events umbenannt: `PropLinesChanged` → `PropertyLinesChanged`,
      `TitelTextChanged` → `PropertyTitelTextChanged`.
    - Methoden umbenannt: `IniFileContentView_LinesChanged` →
      `ContentView_LinesChanged`, `IniFileCommentEdit_TitelTextChanged` →
      `ContentView_TitelTextChanged`.
    - `Dispose` überarbeitet und in `#Region "überschriebene Methoden"`
      verschoben.
    - XML-Dokumentationen für Methoden und Eigenschaften hinzugefügt.
    - Kommentare überarbeitet und präzisiert.
    - Variablen `GroupBox` und `TextBox` aus `InitializeComponent` entfernt
      und in `#Region "Variablendefinition"` verschoben.
    - Neuer Abschnitt `#Region "interne Methoden"` für interne Methoden
      hinzugefügt.
    - `InitializeComponent` um Initialisierung von `GroupBox` und `TextBox`
      ergänzt.

--- SchlumpfSoftControls2/IniFileControl/ContentView.vb
+++ SchlumpfSoftControls2/IniFileControl/ContentView.vb
@@ -2,11 +2,6 @@
 ' Copyright (c) 2025 by Andreas Sauer 
 ' *************************************************************************************************
 
-' TODO: Code noch überarbeiten
-
-Imports System.ComponentModel
-Imports System.Drawing
-
 Namespace IniFileControl
 
     ''' <summary>
@@ -21,36 +16,69 @@ Namespace IniFileControl
     '''   und in <c>InitializeComponent()</c> initialisiert.
     ''' </remarks>
     <ProvideToolboxControl("SchlumpfSoft Controls", False)> ' Sorgt dafür, dass das Control in einer angegebenen Toolbox-Kategorie erscheint.
-    <Description("Steuerelement zum Anzeigen des Dateiinhaltes.")> ' Beschreibt das Control im Designer (Eigenschaftenfenster/Toolbox).
-    <ToolboxItem(True)> ' Markiert die Klasse als Toolbox-Element.
-    <ToolboxBitmap(GetType(IniFileControl.ContentView), "ContentView.bmp")> ' Legt das Symbol in der Toolbox fest.
+    <System.ComponentModel.Description("Steuerelement zum Anzeigen des Dateiinhaltes.")> ' Beschreibt das Control im Designer (Eigenschaftenfenster/Toolbox).
+    <System.ComponentModel.ToolboxItem(True)> ' Markiert die Klasse als Toolbox-Element.
+    <System.Drawing.ToolboxBitmap(GetType(IniFileControl.ContentView), "ContentView.bmp")> ' Legt das Symbol in der Toolbox fest.
     Public Class ContentView : Inherits System.Windows.Forms.UserControl
 
-        ' Hält die aktuell gesetzten Textzeilen, die in der TextBox angezeigt werden.
+#Region "Variablendefinition"
+
+        ''' <summary>
+        ''' Umschließende GroupBox, in der die TextBox angezeigt wird und deren Titelzeile über <see cref="TitelText"/> gesteuert wird.
+        ''' </summary>
+        Private WithEvents GroupBox As System.Windows.Forms.GroupBox
+
+        ''' <summary>
+        ''' ReadOnly-TextBox, in der die Zeilen von <see cref="Lines"/> dargestellt werden.
+        ''' </summary>
+        Private WithEvents TextBox As System.Windows.Forms.TextBox
+
+        ''' <summary>
+        ''' Vom Windows Forms Designer verwaltete Komponentenliste.
+        ''' </summary>
+        Private ReadOnly components As System.ComponentModel.IContainer
+
+        ''' <summary>
+        ''' Hält die aktuell gesetzten Textzeilen, die in der TextBox angezeigt werden.
+        ''' Jede Array-Position entspricht einer Zeile.
+        ''' </summary>
         Private _Lines As String()
 
-        ' Hält den anzuzeigenden Titeltext der GroupBox.
+        ''' <summary>
+        ''' Hält den anzuzeigenden Titeltext der GroupBox.
+        ''' </summary>
         Private _TitelText As String
 
-        ' Internes Event, das ausgelöst wird, wenn sich die Lines-Eigenschaft ändert.
-        ' Die zugehörige Handler-Methode aktualisiert die TextBox.
-        Private Event PropLinesChanged()
+#End Region
 
-        ' Internes Event, das ausgelöst wird, wenn sich der Titeltext ändert.
-        ' Die zugehörige Handler-Methode aktualisiert die GroupBox.
-        Private Event TitelTextChanged()
+#Region "Eventdefinition"
+
+        ''' <summary>
+        ''' Internes Ereignis das ausgelöst wird, wenn sich <see cref="Lines"/> verändert hat und der Inhalt der TextBox aktualisiert werden soll.
+        ''' </summary>
+        Private Event PropertyLinesChanged()
+
+        ''' <summary>
+        ''' Internes Ereignis das ausgelöst wird, wenn sich <see cref="TitelText"/> verändert hat und die GroupBox ihren Titel aktualisieren soll.
+        ''' </summary>
+        Private Event PropertyTitelTextChanged()
+
+#End Region
+
+#Region "öffentliche Methoden"
 
         ''' <summary>
         ''' Initialisiert eine neue Instanz des Controls und stellt Designer-Komponenten bereit.
         ''' </summary>
+        ''' <remarks>
+        ''' Ruft <see cref="InitializeComponent"/> auf und übernimmt den initialen Text der GroupBox in <see cref="TitelText"/>.
+        ''' </remarks>
         Public Sub New()
-            ' Dieser Aufruf initialisiert alle über den Designer erzeugten Steuerelemente (z. B. GroupBox, TextBox).
-            Me.InitializeComponent()
-
-            ' Initialer Titeltext wird aus der im Designer gesetzten GroupBox übernommen,
-            ' sodass die Eigenschaft <see cref="TitelText"/> konsistent ist.
-            Me._TitelText = Me.GroupBox.Text
+            Me.InitializeComponent() ' Wird vom Designer benötigt.
+            Me._TitelText = Me.GroupBox.Text ' Initialer Titeltext sichern.
         End Sub
+
+#End Region
 
 #Region "Definition der neuen Eigenschaften"
 
@@ -59,19 +87,17 @@ Namespace IniFileControl
         ''' </summary>
         ''' <value>Text, der in der GroupBox als Titel angezeigt wird.</value>
         ''' <remarks>
-        ''' - Das Setzen löst das interne Event <c>TitelTextChanged</c> aus, welches die UI aktualisiert.
+        ''' - Das Setzen löst das interne Event <c>PropertyTitelTextChanged</c> aus, welches die UI aktualisiert.
         ''' - Die Property ist im Designer sichtbar und unter "Appearance" kategorisiert.
         ''' </remarks>
-        <Browsable(True)>
-        <Category("Appearance")>
-        <Description("Gibt den Text der Titelzeile zurück oder legt diesen fest.")>
+        <System.ComponentModel.Browsable(True)>
+        <System.ComponentModel.Category("Appearance")>
+        <System.ComponentModel.Description("Gibt den Text der Titelzeile zurück oder legt diesen fest.")>
         Public Property TitelText As String
             Set(value As String)
-                ' Nur reagieren, wenn sich der Wert tatsächlich geändert hat.
                 If Me._TitelText <> value Then
                     Me._TitelText = value
-                    ' UI-Aktualisierung über den zugehörigen Handler anstoßen.
-                    RaiseEvent TitelTextChanged()
+                    RaiseEvent PropertyTitelTextChanged()
                 End If
             End Set
             Get
@@ -84,39 +110,36 @@ Namespace IniFileControl
         ''' </summary>
         ''' <value>Ein String-Array, dessen Elemente als Zeilen in der TextBox angezeigt werden.</value>
         ''' <remarks>
-        ''' - Das Setzen löst das interne Event <c>PropLinesChanged</c> aus, welches die UI aktualisiert.
+        ''' - Das Setzen löst das interne Event <c>PropertyLinesChanged</c> aus, welches die UI aktualisiert.
         ''' - Der Vergleich <c>IsNot</c> prüft hier Referenzungleichheit des Arrays.
         '''   Wird derselbe Array-Instanzinhalt verändert, ohne das Array-Objekt zu tauschen, wird kein Event ausgelöst.
         ''' </remarks>
-        <Browsable(True)>
-        <Category("Appearance")>
-        <Description("Gibt den Dateiinhalt zurück oder legt diesen fest.")>
+        <System.ComponentModel.Browsable(True)>
+        <System.ComponentModel.Category("Appearance")>
+        <System.ComponentModel.Description("Gibt den Dateiinhalt zurück oder legt diesen fest.")>
         Public Property Lines As String()
             Get
                 Return Me._Lines
             End Get
             Set
-                ' Nur reagieren, wenn eine andere Array-Instanz gesetzt wird.
                 If Me._Lines IsNot Value Then
                     Me._Lines = Value
-                    ' UI-Aktualisierung über den zugehörigen Handler anstoßen.
-                    RaiseEvent PropLinesChanged()
+                    RaiseEvent PropertyLinesChanged()
                 End If
             End Set
         End Property
 
 #End Region
 
+#Region "interne Methoden"
+
         ''' <summary>
         ''' Reagiert auf Änderungen der <see cref="Lines"/>-Eigenschaft und überträgt die Zeilen in die TextBox.
         ''' </summary>
         ''' <remarks>
-        ''' - Die Verkabelung erfolgt über <c>Handles Me.PropLinesChanged</c>.
-        ''' - Erwartet, dass <c>TextBox</c> eine im Designer erstellte Instanz ist.
-        ''' - Wenn <c>_Lines</c> <c>Nothing</c> ist, wird der TextBox typischerweise ein leeres Array zugewiesen
-        '''   oder sie bleibt unverändert (abhängig vom WinForms-Verhalten der .NET-Version).
+        ''' Erfolgt durch Zuweisung an <c>TextBox.Lines</c>. Null wird unverändert durchgereicht.
         ''' </remarks>
-        Private Sub IniFileContentView_LinesChanged() Handles Me.PropLinesChanged
+        Private Sub ContentView_LinesChanged() Handles Me.PropertyLinesChanged
             Me.TextBox.Lines = Me._Lines
         End Sub
 
@@ -124,32 +147,18 @@ Namespace IniFileControl
         ''' Reagiert auf Änderungen der <see cref="TitelText"/>-Eigenschaft und aktualisiert den GroupBox-Titel.
         ''' </summary>
         ''' <remarks>
-        ''' - Die Verkabelung erfolgt über <c>Handles Me.TitelTextChanged</c>.
-        ''' - Erwartet, dass <c>GroupBox</c> eine im Designer erstellte Instanz ist.
+        ''' Setzt <c>GroupBox.Text</c> auf den neuen Titel.
         ''' </remarks>
-        Private Sub IniFileCommentEdit_TitelTextChanged() Handles Me.TitelTextChanged
+        Private Sub ContentView_TitelTextChanged() Handles Me.PropertyTitelTextChanged
             Me.GroupBox.Text = Me._TitelText
         End Sub
 
-
-        'UserControl überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
-        <System.Diagnostics.DebuggerNonUserCode()>
-        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            Try
-                If disposing AndAlso components IsNot Nothing Then
-                    components.Dispose()
-                End If
-            Finally
-                MyBase.Dispose(disposing)
-            End Try
-        End Sub
-
-        'Wird vom Windows Form-Designer benötigt.
-        Private components As System.ComponentModel.IContainer
-
-        'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
-        'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
-        'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+        ''' <summary>
+        ''' Initialisiert und konfiguriert alle vom Designer verwalteten Steuerelemente des Controls.
+        ''' </summary>
+        ''' <remarks>
+        ''' Diese Methode wird automatisch vom Konstruktor aufgerufen und sollte nicht manuell geändert werden.
+        ''' </remarks>
         <System.Diagnostics.DebuggerStepThrough()>
         Private Sub InitializeComponent()
             Me.GroupBox = New System.Windows.Forms.GroupBox()
@@ -194,11 +203,26 @@ Namespace IniFileControl
 
         End Sub
 
-        Private WithEvents GroupBox As System.Windows.Forms.GroupBox
-        Private WithEvents TextBox As System.Windows.Forms.TextBox
+#End Region
 
+#Region "überschriebene Methoden"
 
+        ''' <summary>
+        ''' Gibt verwaltete Ressourcen frei und bereinigt die Komponentenliste, sofern vorhanden.
+        ''' </summary>
+        ''' <param name="disposing">True, wenn verwaltete Ressourcen freigegeben werden sollen; andernfalls False.</param>
+        <System.Diagnostics.DebuggerNonUserCode()>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            Try
+                If disposing AndAlso components IsNot Nothing Then
+                    components.Dispose()
+                End If
+            Finally
+                MyBase.Dispose(disposing)
+            End Try
+        End Sub
 
+#End Region
 
     End Class
 
