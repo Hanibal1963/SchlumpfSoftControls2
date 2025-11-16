a83c5eb862685f680e5b49801b0be96384caffc0
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-16 17:20:40 +0100

    Refaktorierung und Erweiterung von DeleteItemDialog
    - "TODO"-Kommentar entfernt und Importe bereinigt.
    - Klasse in Regionen (#Region) unterteilt für bessere Übersicht.
    - Neue Variablen hinzugefügt: ButtonYes, ButtonNo, Label, components.
    - Neue Eigenschaft `ItemValue` zur Verwaltung des Elementwerts.
    - Konstruktor `New` zur Initialisierung der Steuerelemente ergänzt.
    - Methode `Button_Click` überarbeitet und XML-Dokumentation hinzugefügt.
    - `Dispose` überschrieben, um Ressourcen korrekt freizugeben.
    - `InitializeComponent` angepasst: Anker-Eigenschaft und Layout-Definitionen.
    - Ursprüngliche Felder durch neue Definitionen ersetzt.
    - Code-Struktur durch Regionen wie "Variablendefinition" und "öffentliche Methoden" verbessert.

--- SchlumpfSoftControls2/IniFileControl/DeleteItemDialog.vb
+++ SchlumpfSoftControls2/IniFileControl/DeleteItemDialog.vb
@@ -3,12 +3,6 @@
 ' Copyright (c) 2025 by Andreas Sauer 
 ' *************************************************************************************************
 
-
-' TODO: Code noch überarbeiten
-
-Imports System
-Imports System.Windows.Forms
-
 Namespace IniFileControl
 
     ''' <summary>
@@ -24,14 +18,44 @@ Namespace IniFileControl
     ''' </remarks>
     Public Class DeleteItemDialog : Inherits System.Windows.Forms.Form
 
-        ' Hält den aktuell anzuzeigenden Wert des zu löschenden Elements.
-        ' Initialisiert als leere Zeichenfolge.
-        Private _ItemValue As String = $""
+#Region "Variablendefinition"
 
         ''' <summary>
-        ''' Gibt den Wert des Elements zurück oder legt ihn fest.
-        ''' Beim Setzen wird ein Ereignis ausgelöst, um abhängige UI-Elemente zu aktualisieren.
+        ''' Bestätigungs-Schaltfläche für das Löschen ("Ja"). Löst bei Klick den Dialogabschluss mit <see cref="System.Windows.Forms.DialogResult.OK"/> aus.
         ''' </summary>
+        Private WithEvents ButtonYes As System.Windows.Forms.Button
+
+        ''' <summary>
+        ''' Abbruch-Schaltfläche ("Nein"). Löst bei Klick den Dialogabschluss mit <see cref="System.Windows.Forms.DialogResult.Cancel"/> aus.
+        ''' </summary>
+        Private WithEvents ButtonNo As System.Windows.Forms.Button
+
+        ''' <summary>
+        ''' Anzeige-Label für die Bestätigungsfrage, inkl. des aktuell gesetzten Elementwertes.
+        ''' </summary>
+        Private WithEvents Label As System.Windows.Forms.Label
+
+        ''' <summary>
+        ''' Container für untergeordnete Komponenten. Wird vom Windows Forms-Designer verwaltet und beim Dispose freigegeben.
+        ''' </summary>
+        ''' <remarks>ReadOnly, da nur während der Initialisierung gesetzt.</remarks>
+        Private ReadOnly components As System.ComponentModel.IContainer
+
+        ''' <summary>
+        ''' Aktuell anzuzeigender Wert des zu löschenden Elements. Initialwert ist eine leere Zeichenfolge.
+        ''' </summary>
+        ''' <remarks>Wird über die Eigenschaft <see cref="ItemValue"/> gesetzt.</remarks>
+        Private _ItemValue As String = $""
+
+#End Region
+
+#Region "neue Eigenschaften"
+
+        ''' <summary>
+        ''' Wert (Text) des zu löschenden Elements. Wird in der Bestätigungsfrage angezeigt.
+        ''' </summary>
+        ''' <value>Der Elementwert, der im Dialog referenziert wird.</value>
+        ''' <remarks>Beim Setzen wird der UI-Text des Labels aktualisiert.</remarks>
         Public Property ItemValue As String
             Get
                 Return Me._ItemValue
@@ -42,40 +66,45 @@ Namespace IniFileControl
             End Set
         End Property
 
+#End Region
+
+#Region "öffentliche Methoden"
+
         ''' <summary>
-        ''' Gemeinsamer Klick-Handler für die Buttons "Ja" und "Nein".
-        ''' Setzt das entsprechende DialogResult und schließt den Dialog.
+        ''' Erstellt eine neue Instanz des <see cref="DeleteItemDialog"/> und initialisiert Steuerelemente.
         ''' </summary>
-        ''' <param name="sender">Auslösender Button (ButtonYes oder ButtonNo).</param>
-        ''' <param name="e">Ereignisdaten (nicht verwendet).</param>
-        Private Sub Button_Click(sender As Object, e As EventArgs) Handles ButtonYes.Click, ButtonNo.Click
+        ''' <remarks>Die UI-Elemente werden in <see cref="InitializeComponent"/> erzeugt.</remarks>
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+#End Region
+
+#Region "interne Methoden"
+
+        ''' <summary>
+        ''' Gemeinsamer Klick-Handler für die Schaltflächen "Ja" und "Nein".
+        ''' </summary>
+        ''' <param name="sender">Der auslösende Button (<see cref="ButtonYes"/> oder <see cref="ButtonNo"/>).</param>
+        ''' <param name="e">Ereignisdaten (werden hier nicht verwendet).</param>
+        ''' <remarks>
+        ''' Setzt je nach gedrückter Schaltfläche das entsprechende <see cref="System.Windows.Forms.DialogResult"/> und schließt das Formular.
+        ''' </remarks>
+        Private Sub Button_Click(sender As Object, e As System.EventArgs) Handles ButtonYes.Click, ButtonNo.Click
             If sender Is Me.ButtonYes Then ' Welcher Button wurde gedrückt?
-                Me.DialogResult = DialogResult.OK ' Ergebnis setzen
+                Me.DialogResult = System.Windows.Forms.DialogResult.OK ' Ergebnis setzen
             ElseIf sender Is Me.ButtonNo Then
-                Me.DialogResult = DialogResult.Cancel ' Ergebnis setzen
+                Me.DialogResult = System.Windows.Forms.DialogResult.Cancel ' Ergebnis setzen
             End If
             Me.Close() ' Dialog schließen
         End Sub
 
-
-        'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
-        <System.Diagnostics.DebuggerNonUserCode()>
-        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            Try
-                If disposing AndAlso components IsNot Nothing Then
-                    components.Dispose()
-                End If
-            Finally
-                MyBase.Dispose(disposing)
-            End Try
-        End Sub
-
-        'Wird vom Windows Form-Designer benötigt.
-        Private components As System.ComponentModel.IContainer
-
-        'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
-        'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
-        'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+        ''' <summary>
+        ''' Initialisiert alle Steuerelemente des Dialogs und deren Eigenschaften / Layout.
+        ''' </summary>
+        ''' <remarks>
+        ''' Methode wird vom Konstruktor aufgerufen. Änderungen sollten über den Designer erfolgen.
+        ''' </remarks>
         <System.Diagnostics.DebuggerStepThrough()>
         Private Sub InitializeComponent()
             Dim TableLayoutPanel As System.Windows.Forms.TableLayoutPanel
@@ -88,16 +117,16 @@ Namespace IniFileControl
             '
             'TableLayoutPanel
             '
-            TableLayoutPanel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            TableLayoutPanel.Anchor = CType(System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right, System.Windows.Forms.AnchorStyles)
             TableLayoutPanel.ColumnCount = 2
-            TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-            TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+            Dim unused2 = TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+            Dim unused1 = TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
             TableLayoutPanel.Controls.Add(Me.ButtonYes, 0, 0)
             TableLayoutPanel.Controls.Add(Me.ButtonNo, 1, 0)
             TableLayoutPanel.Location = New System.Drawing.Point(149, 40)
             TableLayoutPanel.Name = "TableLayoutPanel"
             TableLayoutPanel.RowCount = 1
-            TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+            Dim unused = TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
             TableLayoutPanel.Size = New System.Drawing.Size(146, 29)
             TableLayoutPanel.TabIndex = 0
             '
@@ -151,13 +180,29 @@ Namespace IniFileControl
 
         End Sub
 
-        Private WithEvents ButtonYes As System.Windows.Forms.Button
-        Private WithEvents ButtonNo As System.Windows.Forms.Button
-        Private WithEvents Label As System.Windows.Forms.Label
+#End Region
 
-        Public Sub New()
-            InitializeComponent()
+#Region "überschriebene Methoden"
+
+        ''' <summary>
+        ''' Gibt die vom Dialog verwendeten Ressourcen frei.
+        ''' </summary>
+        ''' <param name="disposing"><c>True</c>, wenn verwaltete Ressourcen freigegeben werden sollen; andernfalls <c>False</c>.</param>
+        ''' <remarks>
+        ''' Überschreibt <see cref="System.Windows.Forms.Form.Dispose(Boolean)"/> zur Freigabe des Komponenten-Containers.
+        ''' </remarks>
+        <System.Diagnostics.DebuggerNonUserCode()>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            Try
+                If disposing AndAlso components IsNot Nothing Then
+                    components.Dispose()
+                End If
+            Finally
+                MyBase.Dispose(disposing)
+            End Try
         End Sub
+
+#End Region
 
     End Class
 
