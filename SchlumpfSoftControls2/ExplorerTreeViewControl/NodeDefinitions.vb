f152be89d396c0bea2a9d6345b6dfa68416640de
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-21 19:45:52 +0100

    Code-Refactoring und Konsistenzverbesserungen
    - Konsistente Verwendung von `Me` für Instanzvariablen/-methoden.
    - Rückgabewerte von `Nodes.Add` explizit ignoriert (`Dim unused`).
    - Entfernen von `<SuppressMessage>`-Attributen durch obige Anpassung.
    - Kommentare prägnanter und direkt neben den relevanten Code verschoben.
    - Konsistente Nutzung von `Me.Nodes.Clear` und Platzhalterknoten.
    - Fehlerbehandlung in `LoadSubfolders` strukturell optimiert.
    - Anpassung von `FullPath` auf `Me.Tag.ToString()` für Klarheit.
    - Redundante Leerzeilen entfernt, Code gestrafft.
    - Konstruktoren (`DriveNode`, `FolderNode`, `SpecialFolderNode`) vereinheitlicht.
    - Methoden wie `LoadSpecialFolders` und `LoadDrives` klarer strukturiert.
    - Einheitliche Verwendung von `Me` in der gesamten Datei.

--- SchlumpfSoftControls2/ExplorerTreeViewControl/NodeDefinitions.vb
+++ SchlumpfSoftControls2/ExplorerTreeViewControl/NodeDefinitions.vb
@@ -3,8 +3,6 @@
 ' Copyright (c) 2025 by Andreas Sauer 
 ' *************************************************************************************************
 
-' TODO: Code noch überarbeiten
-
 Namespace ExplorerTreeViewControl
 
     ''' <summary>
@@ -21,19 +19,13 @@ Namespace ExplorerTreeViewControl
         ''' <remarks></remarks>        
         Public Sub New()
 
-            'Setze den Text des Knotens mit dem Computernamen
-            Text = $"Dieser Computer ({System.Environment.MachineName})"
-
-            ' Setze das Icon für den Knoten
-            Dim key As String = GetImageKey(ICON_COMPUTER)
-            ImageKey = key
-            SelectedImageKey = key
-
-            ' Leert die Knoten, um Platz für spezielle Ordner und Laufwerke zu schaffen
-            Nodes.Clear()
-
+            Me.Text = $"Dieser Computer ({System.Environment.MachineName})" 'Setze den Text des Knotens mit dem Computernamen
+            Dim key As String = GetImageKey(ICON_COMPUTER) ' Setze das Icon für den Knoten
+            Me.ImageKey = key
+            Me.SelectedImageKey = key
+            Me.Nodes.Clear() ' Leert die Knoten, um Platz für spezielle Ordner und Laufwerke zu schaffen
             ' Füge Platzhalterknoten hinzu, die später durch spezielle Ordner und Laufwerke ersetzt werden
-            Nodes.AddRange({
+            Me.Nodes.AddRange({
                 New System.Windows.Forms.TreeNode("Spezielle Ordner laden ..."),
                 New System.Windows.Forms.TreeNode("Laufwerke laden ...")
                 })
@@ -46,27 +38,25 @@ Namespace ExplorerTreeViewControl
         Public Sub LoadSpecialFolders()
 
             ' Füge spezielle Ordner wie Desktop, Dokumente, Downloads usw. als Knoten hinzu
-            Nodes.AddRange({
+            Me.Nodes.AddRange({
                   New SpecialFolderNode("Desktop"),
                   New SpecialFolderNode("Dokumente"),
                   New SpecialFolderNode("Downloads"),
                   New SpecialFolderNode("Musik"),
                   New SpecialFolderNode("Bilder"),
                   New SpecialFolderNode("Videos")})
+
         End Sub
 
         ''' <summary>
         ''' Lädt die Laufwerke des Computers und fügt sie dem Knoten hinzu.
         ''' </summary>
-        <System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub LoadDrives()
 
             ' Iteriere über alle verfügbaren Laufwerke und füge sie als Knoten hinzu
             For Each drive As System.IO.DriveInfo In System.IO.DriveInfo.GetDrives()
-
                 Dim driveNode As New DriveNode(drive)
-                Nodes.Add(driveNode)
-
+                Dim unused = Me.Nodes.Add(driveNode)
             Next
 
         End Sub
@@ -84,7 +74,7 @@ Namespace ExplorerTreeViewControl
         ''' <remarks>Diese Eigenschaft gibt den Pfad des Laufwerks zurück, das im Tag gespeichert ist.</remarks>
         Public Overloads ReadOnly Property FullPath As String
             Get
-                Return Tag.ToString()
+                Return Me.Tag.ToString()
             End Get
         End Property
 
@@ -94,7 +84,7 @@ Namespace ExplorerTreeViewControl
         ''' <remarks>Diese Eigenschaft verwendet <see cref="DriveInfo"/>, um den Typ des Laufwerks zu ermitteln.</remarks>
         Public ReadOnly Property DriveType As System.IO.DriveType
             Get
-                Return New System.IO.DriveInfo(Tag.ToString()).DriveType
+                Return New System.IO.DriveInfo(Me.Tag.ToString()).DriveType
             End Get
         End Property
 
@@ -103,30 +93,17 @@ Namespace ExplorerTreeViewControl
         ''' </summary>
         ''' <param name="Drive">Laufwerk für welches diese Instanz erstellt werden soll als <see cref="DriveInfo"/></param>
         ''' <remarks></remarks>
-        <System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub New(Drive As System.IO.DriveInfo)
 
-            ' Setzt den Text des Knotens auf das Laufwerkslabel und den Laufwerksnamen (z. B. "Lokaler Datenträger (C:)").
-            Text = $"{GetVolumeLabel(Drive)} ({GetDriveName(Drive)})"
-
-            ' Speichert den Laufwerksnamen (z. B. "C:\") im Tag des Knotens.
-            Tag = Drive.Name
-
-            ' Ermittelt den Laufwerkstyp als String (z. B. "Lokaler Datenträger", "CD-Laufwerk").
-            Dim drivetypestring As String = GetDriveTypeString(Drive)
-
-            ' Ermittelt den Schlüssel für das Symbol basierend auf dem Laufwerkstyp.
-            Dim key As String = GetImageKey(drivetypestring)
-
+            Me.Text = $"{GetVolumeLabel(Drive)} ({GetDriveName(Drive)})" ' Setzt den Text des Knotens auf das Laufwerkslabel und den Laufwerksnamen (z. B. "Lokaler Datenträger (C:)").
+            Me.Tag = Drive.Name ' Speichert den Laufwerksnamen (z. B. "C:\") im Tag des Knotens.
+            Dim drivetypestring As String = GetDriveTypeString(Drive) ' Ermittelt den Laufwerkstyp als String (z. B. "Lokaler Datenträger", "CD-Laufwerk").
+            Dim key As String = GetImageKey(drivetypestring) ' Ermittelt den Schlüssel für das Symbol basierend auf dem Laufwerkstyp.
             ' Setzt das Symbol des Knotens (ImageKey) und das Symbol für den ausgewählten Zustand (SelectedImageKey).
-            ImageKey = key
-            SelectedImageKey = key
-
-            ' Leert die Knoten, um Platz für Unterordner zu schaffen
-            Nodes.Clear()
-
-            ' Füge einen Platzhalterknoten hinzu, der später durch die Unterordner ersetzt wird
-            Nodes.Add(New System.Windows.Forms.TreeNode("Ordner laden ..."))
+            Me.ImageKey = key
+            Me.SelectedImageKey = key
+            Me.Nodes.Clear()  ' Leert die Knoten, um Platz für Unterordner zu schaffen
+            Dim unused = Me.Nodes.Add(New System.Windows.Forms.TreeNode("Ordner laden ...")) ' Füge einen Platzhalterknoten hinzu, der später durch die Unterordner ersetzt wird
 
         End Sub
 
@@ -139,36 +116,23 @@ Namespace ExplorerTreeViewControl
         ''' <para>Fehler wie Zugriffsverletzungen oder IO-Probleme werden abgefangen und
         ''' führen nicht zum Abbruch.</para>
         ''' </remarks>
-        <System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub LoadSubfolders()
 
             Try
-
-                ' Erstellt ein DriveInfo-Objekt für das aktuelle Laufwerk
-                Dim drive As New System.IO.DriveInfo(FullPath)
-
+                Dim drive As New System.IO.DriveInfo(Me.FullPath) ' Erstellt ein DriveInfo-Objekt für das aktuelle Laufwerk
                 ' Prüft, ob das Laufwerk bereit ist (z. B. CD eingelegt, Netzwerk verbunden)
                 If drive.IsReady Then
-
                     ' Durchläuft alle Unterverzeichnisse des Laufwerks
-                    For Each dir As String In System.IO.Directory.GetDirectories(FullPath)
-
-                        ' Fügt jeden gefundenen Ordner als FolderNode dem Knoten hinzu
-                        Nodes.Add(New FolderNode(System.IO.Path.GetFileName(dir), dir))
-
+                    For Each dir As String In System.IO.Directory.GetDirectories(Me.FullPath)
+                        Dim unused = Me.Nodes.Add(New FolderNode(System.IO.Path.GetFileName(dir), dir)) ' Fügt jeden gefundenen Ordner als FolderNode dem Knoten hinzu
                     Next
-
                 End If
-
             Catch ex As System.UnauthorizedAccessException
                 ' Zugriff verweigert – Ordner wird übersprungen, keine Fehlermeldung
-
             Catch ex As System.IO.IOException
                 ' IO-Fehler – z.B. Laufwerk nicht verfügbar, keine Fehlermeldung
-
             Catch ex As System.Exception
                 ' Allgemeiner Fehler – optional loggen, keine Fehlermeldung
-
             End Try
 
         End Sub
@@ -198,7 +162,7 @@ Namespace ExplorerTreeViewControl
         ''' </remarks>
         Public Overloads ReadOnly Property FullPath As String
             Get
-                Return Tag.ToString()
+                Return Me.Tag.ToString()
             End Get
         End Property
 
@@ -209,25 +173,16 @@ Namespace ExplorerTreeViewControl
         ''' <param name="Text">Text der den Name des Ordners wiederspiegelt.</param>
         ''' <param name="FullPath">Der komplette Pfad auf dem Datenträger für diesen
         ''' Ordner.</param>
-        <System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub New(Text As String, FullPath As String)
 
-            ' Setzt den angezeigten Namen des Knotens
-            Me.Text = Text
-
-            ' Speichert den vollständigen Pfad des Ordners im Tag-Property
-            Tag = FullPath
-
+            Me.Text = Text ' Setzt den angezeigten Namen des Knotens
+            Me.Tag = FullPath ' Speichert den vollständigen Pfad des Ordners im Tag-Property
             ' Holt den Bildschlüssel für das Ordner-Icon und weist ihn zu
             Dim key As String = GetImageKey(ICON_FOLDER_FOLDER)
-            ImageKey = key
-            SelectedImageKey = key
-
-            ' Leert die Knoten, um Platz für Unterordner zu schaffen
-            Nodes.Clear()
-
-            ' Füge einen Platzhalterknoten hinzu, der später durch die Unterordner ersetzt wird
-            Nodes.Add(New System.Windows.Forms.TreeNode("Ordner laden ..."))
+            Me.ImageKey = key
+            Me.SelectedImageKey = key
+            Me.Nodes.Clear()  ' Leert die Knoten, um Platz für Unterordner zu schaffen
+            Dim unused = Me.Nodes.Add(New System.Windows.Forms.TreeNode("Ordner laden ...")) ' Füge einen Platzhalterknoten hinzu, der später durch die Unterordner ersetzt wird
 
         End Sub
 
@@ -238,23 +193,16 @@ Namespace ExplorerTreeViewControl
         ''' Diese Methode wird aufgerufen, wenn der Knoten erweitert wird, um die
         ''' Unterordner des Ordners zu laden.
         ''' </remarks>
-        <System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub LoadSubfolders()
 
             Try
-
                 ' Durchlaufe alle Unterverzeichnisse des aktuellen Ordners
-                For Each dir As String In System.IO.Directory.GetDirectories(FullPath)
-
-                    ' Füge für jedes Unterverzeichnis einen neuen FolderNode hinzu
-                    Nodes.Add(New FolderNode(System.IO.Path.GetFileName(dir), dir))
-
+                For Each dir As String In System.IO.Directory.GetDirectories(Me.FullPath)
+                    Dim unused = Me.Nodes.Add(New FolderNode(System.IO.Path.GetFileName(dir), dir)) ' Füge für jedes Unterverzeichnis einen neuen FolderNode hinzu
                 Next
-
             Catch ex As System.UnauthorizedAccessException
                 ' Zugriff verweigert – Ordner wird übersprungen
                 ' Hier könnte optional Logging oder eine Benutzerbenachrichtigung erfolgen
-
             End Try
 
         End Sub
@@ -276,7 +224,7 @@ Namespace ExplorerTreeViewControl
         ''' <remarks>Diese Eigenschaft gibt den Pfad des speziellen Ordners zurück, der im Tag gespeichert ist. </remarks>
         Public Overloads ReadOnly Property FullPath As String
             Get
-                Return Tag.ToString()
+                Return Me.Tag.ToString()
             End Get
         End Property
 
@@ -285,56 +233,42 @@ Namespace ExplorerTreeViewControl
         ''' cref="SchlumpfSoft.Controls.ExplorerTreeViewControl.SpecialFolderNode"/>.
         ''' </summary>
         ''' <param name="Text">Text der den Ordner</param>
-        <System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub New(Text As String)
 
-            ' Setzt den angezeigten Text des Knotens auf den übergebenen Namen des Spezialordners
-            Me.Text = Text
-
+            Me.Text = Text ' Setzt den angezeigten Text des Knotens auf den übergebenen Namen des Spezialordners
             ' Speichert den vollständigen Pfad des Spezialordners im Tag-Property des Knotens
             ' Die Methode GetSpezialFolderPath(Text) ermittelt den Pfad basierend auf dem Namen des Spezialordners (z.B. "Desktop")
-            Tag = GetSpezialFolderPath(Text)
-
+            Me.Tag = GetSpezialFolderPath(Text)
             ' Ermittelt den Schlüssel für das anzuzeigende Symbol (ImageKey) anhand des Ordnernamens
             ' Die Hilfsmethode NodeHelpers.GetImageKey(Text) liefert einen passenden Schlüssel für die Bildliste
             Dim key As String = GetImageKey(Text)
-
             ' Setzt das Symbol des Knotens auf den ermittelten Schlüssel
-            ImageKey = key
-            SelectedImageKey = key
-
-            ' Entfernt alle vorhandenen untergeordneten Knoten, um Platz für die später geladenen Unterordner zu schaffen
-            Nodes.Clear()
-
+            Me.ImageKey = key
+            Me.SelectedImageKey = key
+            Me.Nodes.Clear() ' Entfernt alle vorhandenen untergeordneten Knoten, um Platz für die später geladenen Unterordner zu schaffen
             ' Fügt einen Platzhalterknoten hinzu, der dem Benutzer anzeigt, dass die Unterordner noch geladen werden
             ' Dieser Platzhalter wird später durch die tatsächlichen Unterordner ersetzt, sobald diese geladen wurden
-            Nodes.Add(New System.Windows.Forms.TreeNode("Ordner laden ..."))
+            Dim unused = Me.Nodes.Add(New System.Windows.Forms.TreeNode("Ordner laden ..."))
 
         End Sub
 
         ''' <summary>
         ''' Lädt die Unterordner des speziellen Ordners und fügt sie dem Knoten hinzu.
         ''' </summary>
-        <System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Der Ausdruckswert wird niemals verwendet.", Justification:="<Ausstehend>")>
         Public Sub LoadSubfolders()
 
             ' Versucht, die Unterordner des angegebenen Spezialordners zu laden
             Try
-
                 ' Durchläuft alle Verzeichnisse (Unterordner) im Pfad des Spezialordners
-                For Each dir As String In System.IO.Directory.GetDirectories(FullPath)
-
+                For Each dir As String In System.IO.Directory.GetDirectories(Me.FullPath)
                     ' Fügt für jeden gefundenen Unterordner einen neuen FolderNode zum aktuellen Knoten hinzu
                     ' IO.Path.GetFileName(dir) extrahiert den Ordnernamen aus dem vollständigen Pfad
                     ' "dir" ist der vollständige Pfad des Unterordners
-                    Nodes.Add(New FolderNode(System.IO.Path.GetFileName(dir), dir))
-
+                    Dim unused = Me.Nodes.Add(New FolderNode(System.IO.Path.GetFileName(dir), dir))
                 Next
-
             Catch ex As System.UnauthorizedAccessException
                 ' Falls der Zugriff auf einen Ordner verweigert wird, wird die Ausnahme abgefangen
                 ' und der entsprechende Ordner übersprungen, ohne die Anwendung zu unterbrechen
-
             End Try
 
         End Sub
