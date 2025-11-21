5698aff9800eba8691f798b8eac7883000a6d541
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-21 19:25:58 +0100

    Neue Konstanten für Laufwerkstypen und Ordner hinzugefügt
    - Mehrere Konstanten in `ConstantDefinitions.vb` hinzugefügt,
      um Schlüssel und Beschreibungen für Laufwerkstypen,
      spezielle Ordner und Symbole zu definieren.
    - Im Modul `DriveTypeKeyConstantDefinitions` wurden Schlüssel
      für Laufwerkstypen wie `DRIVETYPE_FIXED`, `DRIVETYPE_CDROM`
      und weitere ergänzt.
    - Im Modul `DriveDescConstantDefinitions` wurden Anzeigenamen
      für Laufwerkstypen wie `DRIVE_DESC_FIXED`, `DRIVE_DESC_CDROM`
      und weitere hinzugefügt.
    - Im Modul `FolderNamesKeyConstantDefinitions` wurden Schlüssel
      für spezielle Ordner wie `FOLDER_COMPUTER`, `FOLDER_DESKTOP`
      und weitere ergänzt.
    - Im Modul `IconKeyConstantDefinitions` wurden Symbolschlüssel
      für Laufwerke und Ordner wie `ICON_COMPUTER`, `ICON_DRIVE_FIXED`
      und weitere hinzugefügt.
    - XML-Kommentare für jede Konstante hinzugefügt, um deren Zweck
      zu dokumentieren.
    - Der Kommentar `TODO: Code noch überarbeiten` wurde entfernt,
      da die Überarbeitung abgeschlossen wurde.

--- SchlumpfSoftControls2/ExplorerTreeViewControl/ConstantDefinitions.vb
+++ SchlumpfSoftControls2/ExplorerTreeViewControl/ConstantDefinitions.vb
@@ -3,8 +3,6 @@
 ' Copyright (c) 2025 by Andreas Sauer 
 ' *************************************************************************************************
 
-' TODO: Code noch überarbeiten
-
 Namespace ExplorerTreeViewControl
 
     ''' <summary>
@@ -12,14 +10,49 @@ Namespace ExplorerTreeViewControl
     ''' </summary>
     Friend Module DriveTypeKeyConstantDefinitions
 
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: Lokaler Datenträger
+        ''' </summary>
         Public Const DRIVETYPE_FIXED As String = "Fixed"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: CD/DVD/BD-Laufwerk
+        ''' </summary>
         Public Const DRIVETYPE_CDROM As String = "CDROM"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: Wechselmedium (z. B. USB-Stick)
+        ''' </summary>
         Public Const DRIVETYPE_REMOVABLE As String = "Removable"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: Netzlaufwerk
+        ''' </summary>
         Public Const DRIVETYPE_NETWORK As String = "Network"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: RAM-Disk
+        ''' </summary>
         Public Const DRIVETYPE_RAM As String = "RamDisk"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: Kein Root-Verzeichnis vorhanden
+        ''' </summary>
         Public Const DRIVETYPE_NOROOT As String = "NoRoot"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: Unbekannt
+        ''' </summary>
         Public Const DRIVETYPE_UNKNOWN As String = "Unknown"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: Systemlaufwerk
+        ''' </summary>
         Public Const DRIVETYPE_SYSTEM As String = "System"
+
+        ''' <summary>
+        ''' Schlüssel für Laufwerkstyp: Diskettenlaufwerk
+        ''' </summary>
         Public Const DRIVETYPE_FLOPPY As String = "Floppy"
 
     End Module
@@ -29,13 +62,44 @@ Namespace ExplorerTreeViewControl
     ''' </summary>
     Friend Module DriveDescConstantDefinitions
 
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: Lokaler Datenträger
+        ''' </summary>
         Public Const DRIVE_DESC_FIXED As String = "Lokaler Datenträger"
+
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: CD-Laufwerk
+        ''' </summary>
         Public Const DRIVE_DESC_CDROM As String = "CD-Laufwerk"
+
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: Diskettenlaufwerk
+        ''' </summary>
         Public Const DRIVE_DESC_FLOPPY As String = "Diskettenlaufwerk"
+
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: Wechselmedium
+        ''' </summary>
         Public Const DRIVE_DESC_REMOVABLE As String = "Wechselmedium"
+
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: Netzlaufwerk
+        ''' </summary>
         Public Const DRIVE_DESC_NETWORK As String = "Netzlaufwerk"
+
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: RAM-Laufwerk
+        ''' </summary>
         Public Const DRIVE_DESC_RAM As String = "Ramlaufwerk"
+
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: Kein Root-Verzeichnis
+        ''' </summary>
         Public Const DRIVE_DESC_NOROOT As String = "kein Root-Verzeichnis"
+
+        ''' <summary>
+        ''' Anzeigename für Laufwerkstyp: Unbekanntes Laufwerk
+        ''' </summary>
         Public Const DRIVE_DESC_UNKNOWN As String = "Unbekanntes Laufwerk"
 
     End Module
@@ -45,13 +109,44 @@ Namespace ExplorerTreeViewControl
     ''' </summary>
     Friend Module FolderNamesKeyConstantDefinitions
 
+        ''' <summary>
+        ''' Schlüssel für speziellen Ordner: Computer
+        ''' </summary>
         Public Const FOLDER_COMPUTER As String = "Computer"
+
+        ''' <summary>
+        ''' Schlüssel für speziellen Ordner: Desktop
+        ''' </summary>
         Public Const FOLDER_DESKTOP As String = "Desktop"
+
+        ''' <summary>
+        ''' Schlüssel für speziellen Ordner: Dokumente
+        ''' </summary>
         Public Const FOLDER_DOKUMENTE As String = "Dokumente"
+
+        ''' <summary>
+        ''' Schlüssel für speziellen Ordner: Downloads
+        ''' </summary>
         Public Const FOLDER_DOWNLOADS As String = "Downloads"
+
+        ''' <summary>
+        ''' Schlüssel für speziellen Ordner: Musik
+        ''' </summary>
         Public Const FOLDER_MUSIK As String = "Musik"
+
+        ''' <summary>
+        ''' Schlüssel für speziellen Ordner: Bilder
+        ''' </summary>
         Public Const FOLDER_BILDER As String = "Bilder"
+
+        ''' <summary>
+        ''' Schlüssel für speziellen Ordner: Videos
+        ''' </summary>
         Public Const FOLDER_VIDEOS As String = "Videos"
+
+        ''' <summary>
+        ''' Schlüssel für allgemeinen Ordner
+        ''' </summary>
         Public Const FOLDER_FOLDER As String = "Folder"
 
     End Module
@@ -61,22 +156,89 @@ Namespace ExplorerTreeViewControl
     ''' </summary>
     Friend Module IconKeyConstantDefinitions
 
+        ''' <summary>
+        ''' Symbolschlüssel: Computer
+        ''' </summary>
         Public Const ICON_COMPUTER As String = "Computer"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Ordner Desktop
+        ''' </summary>
         Public Const ICON_FOLDER_DESKTOP As String = "FolderDesktop"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Ordner Dokumente
+        ''' </summary>
         Public Const ICON_FOLDER_DOCUMENTS As String = "FolderDocuments"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Ordner Downloads
+        ''' </summary>
         Public Const ICON_FOLDER_DOWNLOADS As String = "FolderDownloads"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Ordner Musik
+        ''' </summary>
         Public Const ICON_FOLDER_MUSIC As String = "FolderMusic"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Ordner Bilder
+        ''' </summary>
         Public Const ICON_FOLDER_PICTURES As String = "FolderPictures"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Ordner Videos
+        ''' </summary>
         Public Const ICON_FOLDER_VIDEOS As String = "FolderVideos"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Allgemeiner Ordner
+        ''' </summary>
         Public Const ICON_FOLDER_FOLDER As String = "Folder"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Systemlaufwerk
+        ''' </summary>
         Public Const ICON_DRIVE_SYSTEM As String = "DriveSystem"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Lokaler Datenträger
+        ''' </summary>
         Public Const ICON_DRIVE_FIXED As String = "DriveFixed"
+
+        ''' <summary>
+        ''' Symbolschlüssel: CD/DVD/BD-Laufwerk
+        ''' </summary>
         Public Const ICON_DRIVE_CDROM As String = "DriveCDROM"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Diskettenlaufwerk
+        ''' </summary>
         Public Const ICON_DRIVE_FLOPPY As String = "DriveFloppy"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Wechselmedium
+        ''' </summary>
         Public Const ICON_DRIVE_REMOVABLE As String = "DriveRemovable"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Netzlaufwerk
+        ''' </summary>
         Public Const ICON_DRIVE_NETWORK As String = "DriveNetwork"
+
+        ''' <summary>
+        ''' Symbolschlüssel: RAM-Disk
+        ''' </summary>
         Public Const ICON_DRIVE_RAM As String = "DiveRamDisk"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Kein Root-Verzeichnis
+        ''' </summary>
         Public Const ICON_DRIVE_NOROOT As String = "DriveNoRoot"
+
+        ''' <summary>
+        ''' Symbolschlüssel: Unbekannter Laufwerkstyp
+        ''' </summary>
         Public Const ICON_DRIVE_UNKNOWN As String = "DriveUnknown"
 
     End Module
