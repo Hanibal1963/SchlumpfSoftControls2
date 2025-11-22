939c0abb42ebea8e70587763a6b134ce68c678e7
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-22 13:36:58 +0100

    Neue `FormTemplate`-Klasse und Ressourcen hinzugefügt
    - Neue Ressourcendatei `FormTemplate.resx` hinzugefügt, die XML-Daten für Farben, Bilder und andere Eigenschaften der `FormTemplate`-Klasse enthält.
    - `FormTemplate`-Klasse erstellt, die ein benutzerdefiniertes Benachrichtigungsfenster definiert, inklusive Eigenschaften, Steuerungselementen und Animationen.
    - `NotifyForm.vb` überarbeitet, um die neue `FormTemplate`-Klasse zu verwenden. Alte, eingebettete `FormTemplate`-Klasse entfernt.
    - Projektdatei `SchlumpfSoftControls2.vbproj` aktualisiert, um `FormTemplate.vb` und `FormTemplate.resx` als eingebettete Ressourcen hinzuzufügen.

---
+++ SchlumpfSoftControls2/NotifyFormControl/FormTemplate.vb
@@ -0,0 +1,227 @@
+﻿' *************************************************************************************************
+' FormTemplate.vb
+' Copyright (c) 2025 by Andreas Sauer 
+' *************************************************************************************************
+
+' TODO: Code noch überarbeiten
+
+Namespace NotifyFormControl
+
+    Friend Class FormTemplate : Inherits System.Windows.Forms.Form
+
+        Private CloseThread As System.Threading.Thread
+
+        'Eigenschaften
+        Public Shared BackgroundColor As System.Drawing.Color
+        Public Shared FontColor As System.Drawing.Color
+        Public Shared Image As System.Drawing.Image
+        Public Shared Message As String
+        Public Shared ShowTime As Integer
+        Public Shared TextFieldColor As System.Drawing.Color
+        Public Shared Title As String
+        Public Shared TitleBarColor As System.Drawing.Color
+
+        'Controls
+        Private ReadOnly LabelClose As New System.Windows.Forms.Label
+        Private ReadOnly LabelTitle As New System.Windows.Forms.Label
+        Private ReadOnly PanelSpacer As New System.Windows.Forms.Panel
+        Private ReadOnly PanelTitle As New System.Windows.Forms.Panel
+        Private ReadOnly PictureBoxImage As New System.Windows.Forms.PictureBox
+        Private ReadOnly RichTextBoxMessage As New System.Windows.Forms.RichTextBox
+
+        Private Sub CloseForm()
+            If Me.InvokeRequired Then
+                Dim unused = Me.Invoke(New System.Windows.Forms.MethodInvoker(AddressOf Me.CloseForm))
+            Else
+                Me.Close()
+            End If
+        End Sub
+
+        Public Sub Initialize()
+            With Me
+                .MaximizeBox = False
+                .MinimizeBox = False
+                .BackColor = BackgroundColor
+                .ForeColor = FontColor
+                .Size = New System.Drawing.Size(504, 138)
+                .FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                .StartPosition = System.Windows.Forms.FormStartPosition.Manual
+            End With
+            Me.Show()
+        End Sub
+
+        Protected Overrides Sub Dispose(disposing As Boolean)
+            'Bereinigung durchführen
+            With Me
+                .LabelClose.Dispose()
+                .LabelTitle.Dispose()
+                .PanelSpacer.Dispose()
+                .PanelTitle.Dispose()
+                .PictureBoxImage.Dispose()
+                .RichTextBoxMessage.Dispose()
+            End With
+            MyBase.Dispose(disposing)
+        End Sub
+
+        Private Sub AddControls()
+            With Me.Controls
+                .Add(Me.LabelTitle)
+                .Add(Me.LabelClose)
+                .Add(Me.PanelSpacer)
+                .Add(Me.PanelTitle)
+                .Add(Me.RichTextBoxMessage)
+                .Add(Me.PictureBoxImage)
+            End With
+        End Sub
+
+        Private Sub AutoClose()
+            'Fenster nur automatisch schließen wenn Zeit > 0 ist.
+            If ShowTime > 0 Then
+                System.Threading.Thread.Sleep(ShowTime)
+                Me.CloseForm()
+            End If
+        End Sub
+
+        Private Sub Form_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles Me.Closing
+            System.Threading.Thread.Sleep(150)
+            For iCount As Integer = 90 To 10 Step -15
+                Me.Opacity = iCount / 110
+                Me.Refresh()
+                System.Threading.Thread.Sleep(60)
+            Next
+        End Sub
+
+        Private Sub Form_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+            With Me
+                .Opacity = 0.1
+                .SetPropertys_Lbl_Close()
+                .SetPropertys_Panel_Spacer()
+                .SetPropertys_Pb_Image()
+                .SetPropertys_Panel_Title()
+                .SetPropertys_Lbl_Title()
+                .SetPropertys_Txt_Msg()
+                .AddControls()
+                .ActiveControl = .Controls.Item(1)
+            End With
+            'Ändern Sie die Position in die untere rechte Ecke
+            Dim x As Integer = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width
+            Dim y As Integer = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height - Me.Height - 50
+            Do Until x = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width - Me.Width
+                x -= 1
+                Me.Location = New System.Drawing.Point(x, y)
+            Loop
+            AddHandler Me.LabelClose.Click, AddressOf Me.Lbl_Close_Click
+            Me.FormFadeIn()
+            'Starte Thread für Autoclose-Popup.
+            Me.CloseThread = New System.Threading.Thread(AddressOf Me.AutoClose) With {.IsBackground = True}
+            Me.CloseThread.Start()
+        End Sub
+
+        Private Sub FormFadeIn()
+            For iCount As Integer = 10 To 100 Step +15
+                Me.Opacity = iCount / 100
+                Me.Refresh()
+                System.Threading.Thread.Sleep(60)
+            Next
+        End Sub
+
+        Private Sub Lbl_Close_Click(sender As Object, e As System.EventArgs)
+            Me.Close()
+        End Sub
+
+        Private Sub SetPropertys_Lbl_Close()
+            With Me.LabelClose
+                .AutoSize = True
+                .BackColor = TitleBarColor
+                .ForeColor = FontColor
+                .Location = New System.Drawing.Point(478, 9)
+                .Name = "LblClose"
+                .Size = New System.Drawing.Size(14, 13)
+                .TabIndex = 1
+                .Text = "X"
+            End With
+        End Sub
+
+        Private Sub SetPropertys_Lbl_Title()
+            With Me.LabelTitle
+                .AutoSize = True
+                .BackColor = TitleBarColor
+                .ForeColor = FontColor
+                .Location = New System.Drawing.Point(11, 9)
+                .Name = "LblTitle"
+                .Size = New System.Drawing.Size(27, 13)
+                .TabIndex = 0
+                .Text = Title
+            End With
+        End Sub
+
+        Private Sub SetPropertys_Panel_Spacer()
+            With Me.PanelSpacer
+                .BackColor = TitleBarColor
+                .Location = New System.Drawing.Point(119, 36)
+                .Name = "PanelSpacer"
+                .Size = New System.Drawing.Size(1, 92)
+                .TabIndex = 2
+            End With
+        End Sub
+
+        Private Sub SetPropertys_Panel_Title()
+            With Me.PanelTitle
+                .BackColor = TitleBarColor
+                .Controls.Add(Me.LabelClose)
+                .Controls.Add(Me.LabelTitle)
+                .Location = New System.Drawing.Point(0, -1)
+                .Name = "PanelTitle"
+                .Size = New System.Drawing.Size(505, 29)
+                .TabIndex = 0
+            End With
+        End Sub
+
+        Private Sub SetPropertys_Pb_Image()
+            With Me.PictureBoxImage
+                .BackColor = System.Drawing.Color.Transparent
+                .Location = New System.Drawing.Point(12, 36)
+                .Name = "PicBoxImage"
+                .Size = New System.Drawing.Size(92, 92)
+                .TabIndex = 1
+                .TabStop = False
+                .Image = Image
+                .SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+                .Refresh()
+            End With
+        End Sub
+
+        Private Sub SetPropertys_Txt_Msg()
+            With Me.RichTextBoxMessage
+                .BackColor = TextFieldColor
+                .ForeColor = FontColor
+                .BorderStyle = System.Windows.Forms.BorderStyle.None
+                .Location = New System.Drawing.Point(133, 37)
+                .Multiline = True
+                .Name = "RiTxtMsg"
+                .Size = New System.Drawing.Size(361, 90)
+                .TabIndex = 3
+                .ReadOnly = True
+                .Text = Message
+                .SelectionProtected = True
+                .Cursor = System.Windows.Forms.Cursors.Arrow
+            End With
+        End Sub
+
+        Protected Overrides Sub Finalize()
+            MyBase.Finalize()
+        End Sub
+
+        Private Sub InitializeComponent()
+            Me.SuspendLayout()
+            '
+            'FormTemplate
+            '
+            Me.ClientSize = New System.Drawing.Size(284, 261)
+            Me.Name = "FormTemplate"
+            Me.ResumeLayout(False)
+        End Sub
+
+    End Class
+
+End Namespace
--- SchlumpfSoftControls2/NotifyFormControl/NotifyForm.vb
+++ SchlumpfSoftControls2/NotifyFormControl/NotifyForm.vb
@@ -5,41 +5,33 @@
 
 ' TODO: Code noch überarbeiten
 
-Imports System
-Imports System.ComponentModel
-Imports System.Drawing
-Imports System.Threading
-Imports System.Windows.Forms
-
 Namespace NotifyFormControl
 
     ''' <summary>
     ''' Control zum anzeigen von Benachrichtigungsfenstern.
     ''' </summary>
     <ProvideToolboxControl("SchlumpfSoft Controls", False)>
-    <Description("Control zum anzeigen von Benachrichtigungsfenstern.")>
-    <ToolboxItem(True)>
-    <ToolboxBitmap(GetType(NotifyFormControl.NotifyForm), "NotifyForm.bmp")>
-    Public Class NotifyForm
+    <System.ComponentModel.Description("Control zum anzeigen von Benachrichtigungsfenstern.")>
+    <System.ComponentModel.ToolboxItem(True)>
+    <System.Drawing.ToolboxBitmap(GetType(NotifyFormControl.NotifyForm), "NotifyForm.bmp")>
+    Public Class NotifyForm : Inherits System.ComponentModel.Component
 
-        Inherits Component
-
-#Region "öffentliche Eigenschaften"
+#Region "neue Eigenschaften"
 
         ''' <summary>
         ''' Legt das Aussehen des Benachrichtigungsfensters fest.
         ''' </summary>
-        <Browsable(True)>
-        <Category("Appearance")>
-        <Description("Legt das Aussehen des Benachrichtigungsfensters fest.")>
+        <System.ComponentModel.Browsable(True)>
+        <System.ComponentModel.Category("Appearance")>
+        <System.ComponentModel.Description("Legt das Aussehen des Benachrichtigungsfensters fest.")>
         Public Property Design As NotifyFormDesign
 
         ''' <summary>
         ''' Legt den Benachrichtigungstext fest der angezeigt werden soll.
         ''' </summary>
-        <Browsable(True)>
-        <Category("Appearance")>
-        <Description("Legt den Benachrichtigungstext fest der angezeigt werden soll.")>
+        <System.ComponentModel.Browsable(True)>
+        <System.ComponentModel.Category("Appearance")>
+        <System.ComponentModel.Description("Legt den Benachrichtigungstext fest der angezeigt werden soll.")>
         Public Property Message As String
 
         ''' <summary>
@@ -48,105 +40,102 @@ Namespace NotifyFormControl
         ''' <remarks>
         ''' Der Wert 0 bewirkt das kein automatisches schließen des Fensters erfolgt.
         ''' </remarks>
-        <Browsable(True)>
-        <Category("Behavior")>
-        <Description("Legt die Anzeigedauer des Benachrichtigungsfensters in ms fest.")>
+        <System.ComponentModel.Browsable(True)>
+        <System.ComponentModel.Category("Behavior")>
+        <System.ComponentModel.Description("Legt die Anzeigedauer des Benachrichtigungsfensters in ms fest.")>
         Public Property ShowTime As Integer
 
         ''' <summary>
         ''' Legt das anzuzeigende Symbol im Benachrichtigungsfensters fest.
         ''' </summary>
-        <Browsable(True)>
-        <Category("Appearance")>
-        <Description("Legt das anzuzeigende Symbol im Benachrichtigungsfensters fest.")>
+        <System.ComponentModel.Browsable(True)>
+        <System.ComponentModel.Category("Appearance")>
+        <System.ComponentModel.Description("Legt das anzuzeigende Symbol im Benachrichtigungsfensters fest.")>
         Public Property Style As NotifyFormStyle
 
         ''' <summary>
         ''' Legt den Text der Titelzeile des Benachrichtigungsfensters fest.
         ''' </summary>
-        <Browsable(True)>
-        <Category("Appearance")>
-        <Description("Legt den Text der Titelzeile des Benachrichtigungsfensters fest.")>
+        <System.ComponentModel.Browsable(True)>
+        <System.ComponentModel.Category("Appearance")>
+        <System.ComponentModel.Description("Legt den Text der Titelzeile des Benachrichtigungsfensters fest.")>
         Public Property Title As String
 
 #End Region
 
+#Region "öffentliche Methoden"
+
         Public Sub New()
-            Title = $"Titel"
-            Message = $"Mitteilung"
-            Design = NotifyFormDesign.Bright
-            Style = NotifyFormStyle.Information
-            ShowTime = 5000
+            Me.Title = $"Titel"
+            Me.Message = $"Mitteilung"
+            Me.Design = NotifyFormDesign.Bright
+            Me.Style = NotifyFormStyle.Information
+            Me.ShowTime = 5000
         End Sub
 
         ''' <summary>
         ''' Zeigt das Meldungsfenster an.
         ''' </summary>
-        <Description("Zeigt das Meldungsfenster an.")>
+        <System.ComponentModel.Description("Zeigt das Meldungsfenster an.")>
         Public Sub Show()
-            FormTemplate.Image = SetFormImage()
-            FormTemplate.Title = Title
-            FormTemplate.Message = Message
-            FormTemplate.ShowTime = ShowTime
-            SetFormDesign()
+            FormTemplate.Image = Me.SetFormImage()
+            FormTemplate.Title = Me.Title
+            FormTemplate.Message = Me.Message
+            FormTemplate.ShowTime = Me.ShowTime
+            Me.SetFormDesign()
+            Me.ShowForm()
         End Sub
 
-#Region "Interne Hilfsfunktionen"
+#End Region
+
+#Region "Interne Methoden"
+
+        Private Sub ShowForm()
+            Dim frm As New FormTemplate
+            frm.Initialize()
+        End Sub
 
         ' Setzt das Design des Fensters
         Private Sub SetFormDesign()
-            Select Case Design
-                Case NotifyFormDesign.Bright
-                    SetFormDesignBright()
-                Case NotifyFormDesign.Colorful
-                    SetFormDesignColorful()
-                Case NotifyFormDesign.Dark
-                    SetFormDesignDark()
+            Select Case Me.Design
+                Case NotifyFormDesign.Bright : SetFormDesignBright()
+                Case NotifyFormDesign.Colorful : SetFormDesignColorful()
+                Case NotifyFormDesign.Dark : SetFormDesignDark()
             End Select
         End Sub
 
         ' Setzt das helle Design
         Private Shared Sub SetFormDesignBright()
-            FormTemplate.BackgroundColor = Color.White
-            FormTemplate.TextFieldColor = Color.White
-            FormTemplate.TitleBarColor = Color.Gray
-            FormTemplate.FontColor = Color.Black
-            Dim ini As New FormTemplate
-            ini.Initialize()
+            FormTemplate.BackgroundColor = System.Drawing.Color.White
+            FormTemplate.TextFieldColor = System.Drawing.Color.White
+            FormTemplate.TitleBarColor = System.Drawing.Color.Gray
+            FormTemplate.FontColor = System.Drawing.Color.Black
         End Sub
 
         ' Setz das farbige Design
         Private Shared Sub SetFormDesignColorful()
-            FormTemplate.BackgroundColor = Color.LightBlue
-            FormTemplate.TextFieldColor = Color.LightBlue
-            FormTemplate.TitleBarColor = Color.LightSeaGreen
-            FormTemplate.FontColor = Color.White
-            Dim ini As New FormTemplate
-            ini.Initialize()
+            FormTemplate.BackgroundColor = System.Drawing.Color.LightBlue
+            FormTemplate.TextFieldColor = System.Drawing.Color.LightBlue
+            FormTemplate.TitleBarColor = System.Drawing.Color.LightSeaGreen
+            FormTemplate.FontColor = System.Drawing.Color.White
         End Sub
 
         ' Setzt das dunkle Design
         Private Shared Sub SetFormDesignDark()
-            FormTemplate.BackgroundColor = Color.FromArgb(83, 79, 75)
-            FormTemplate.TextFieldColor = Color.FromArgb(83, 79, 75)
-            FormTemplate.TitleBarColor = Color.FromArgb(60, 57, 54)
-            FormTemplate.FontColor = Color.White
-            Dim ini As New FormTemplate
-            ini.Initialize()
+            FormTemplate.BackgroundColor = System.Drawing.Color.FromArgb(83, 79, 75)
+            FormTemplate.TextFieldColor = System.Drawing.Color.FromArgb(83, 79, 75)
+            FormTemplate.TitleBarColor = System.Drawing.Color.FromArgb(60, 57, 54)
+            FormTemplate.FontColor = System.Drawing.Color.White
         End Sub
 
         ' Setzt das Symbol des Fensters
-        Private Function SetFormImage() As Image
-            Dim result As Bitmap = Nothing
-            Select Case Style
-                Case NotifyFormStyle.CriticalError
-                    result = My.Resources.CriticalError
-                Case NotifyFormStyle.Exclamation
-                    result = My.Resources.Warning
-                Case NotifyFormStyle.Information
-                    result = My.Resources.Information
-                Case NotifyFormStyle.Question
-                    result = My.Resources.Question
+        Private Function SetFormImage() As System.Drawing.Image
+            Dim result As System.Drawing.Bitmap = Nothing
+            Select Case Me.Style
+                Case NotifyFormStyle.CriticalError : result = My.Resources.CriticalError
+                Case NotifyFormStyle.Exclamation : result = My.Resources.Warning
+                Case NotifyFormStyle.Information : result = My.Resources.Information
+                Case NotifyFormStyle.Question : result = My.Resources.Question
             End Select
             Return result
         End Function
@@ -155,226 +144,6 @@ Namespace NotifyFormControl
         End Sub
 
 #End Region
-
-        ' Vorlage für das Benachrichtigungsfenster
-        Private Class FormTemplate
-
-            Inherits Form
-
-            Private CloseThread As Thread
-
-            'Eigenschaften
-            Public Shared BackgroundColor As Color
-            Public Shared FontColor As Color
-            Public Shared Image As Image
-            Public Shared Message As String
-            Public Shared ShowTime As Integer
-            Public Shared TextFieldColor As Color
-            Public Shared Title As String
-            Public Shared TitleBarColor As Color
-
-            'Controls
-            Private ReadOnly LabelClose As New Label
-            Private ReadOnly LabelTitle As New Label
-            Private ReadOnly PanelSpacer As New Panel
-            Private ReadOnly PanelTitle As New Panel
-            Private ReadOnly PictureBoxImage As New PictureBox
-            Private ReadOnly RichTextBoxMessage As New RichTextBox
-
-            Private Sub CloseForm()
-                If InvokeRequired Then
-                    Dim unused = Invoke(New MethodInvoker(AddressOf CloseForm))
-                Else
-                    Close()
-                End If
-            End Sub
-
-            Public Sub Initialize()
-                With Me
-                    .MaximizeBox = False
-                    .MinimizeBox = False
-                    .BackColor = BackgroundColor
-                    .ForeColor = FontColor
-                    .Size = New Size(504, 138)
-                    .FormBorderStyle = FormBorderStyle.None
-                    .StartPosition = FormStartPosition.Manual
-                End With
-                Show()
-            End Sub
-
-            Protected Overrides Sub Dispose(disposing As Boolean)
-                'Bereinigung durchführen
-                With Me
-                    .LabelClose.Dispose()
-                    .LabelTitle.Dispose()
-                    .PanelSpacer.Dispose()
-                    .PanelTitle.Dispose()
-                    .PictureBoxImage.Dispose()
-                    .RichTextBoxMessage.Dispose()
-                End With
-                MyBase.Dispose(disposing)
-            End Sub
-
-            Private Sub AddControls()
-                With Controls
-                    .Add(LabelTitle)
-                    .Add(LabelClose)
-                    .Add(PanelSpacer)
-                    .Add(PanelTitle)
-                    .Add(RichTextBoxMessage)
-                    .Add(PictureBoxImage)
-                End With
-            End Sub
-
-            Private Sub AutoClose()
-                'Fenster nur automatisch schließen wenn Zeit > 0 ist.
-                If ShowTime > 0 Then
-                    Thread.Sleep(ShowTime)
-                    CloseForm()
-                End If
-            End Sub
-
-            Private Sub Form_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles Me.Closing
-                Thread.Sleep(150)
-                For iCount As Integer = 90 To 10 Step -15
-                    Opacity = iCount / 110
-                    Refresh()
-                    Thread.Sleep(60)
-                Next
-            End Sub
-
-            Private Sub Form_Load(sender As Object, e As EventArgs) Handles Me.Load
-                With Me
-                    .Opacity = 0.1
-                    .SetPropertys_Lbl_Close()
-                    .SetPropertys_Panel_Spacer()
-                    .SetPropertys_Pb_Image()
-                    .SetPropertys_Panel_Title()
-                    .SetPropertys_Lbl_Title()
-                    .SetPropertys_Txt_Msg()
-                    .AddControls()
-                    .ActiveControl = .Controls.Item(1)
-                End With
-                'Ändern Sie die Position in die untere rechte Ecke
-                Dim x As Integer = Screen.PrimaryScreen.WorkingArea.Width
-                Dim y As Integer = Screen.PrimaryScreen.WorkingArea.Height - Height - 50
-                Do Until x = Screen.PrimaryScreen.WorkingArea.Width - Width
-                    x -= 1
-                    Location = New Point(x, y)
-                Loop
-                AddHandler LabelClose.Click, AddressOf Lbl_Close_Click
-                FormFadeIn()
-                'Starte Thread für Autoclose-Popup.
-                CloseThread = New Thread(AddressOf AutoClose) With {.IsBackground = True}
-                CloseThread.Start()
-            End Sub
-
-            Private Sub FormFadeIn()
-                For iCount As Integer = 10 To 100 Step +15
-                    Opacity = iCount / 100
-                    Refresh()
-                    Thread.Sleep(60)
-                Next
-            End Sub
-
-            Private Sub Lbl_Close_Click(sender As Object, e As EventArgs)
-                Close()
-            End Sub
-
-            Private Sub SetPropertys_Lbl_Close()
-                With LabelClose
-                    .AutoSize = True
-                    .BackColor = TitleBarColor
-                    .ForeColor = FontColor
-                    .Location = New Point(478, 9)
-                    .Name = "LblClose"
-                    .Size = New Size(14, 13)
-                    .TabIndex = 1
-                    .Text = "X"
-                End With
-            End Sub
-
-            Private Sub SetPropertys_Lbl_Title()
-                With LabelTitle
-                    .AutoSize = True
-                    .BackColor = TitleBarColor
-                    .ForeColor = FontColor
-                    .Location = New Point(11, 9)
-                    .Name = "LblTitle"
-                    .Size = New Size(27, 13)
-                    .TabIndex = 0
-                    .Text = Title
-                End With
-            End Sub
-
-            Private Sub SetPropertys_Panel_Spacer()
-                With PanelSpacer
-                    .BackColor = TitleBarColor
-                    .Location = New Point(119, 36)
-                    .Name = "PanelSpacer"
-                    .Size = New Size(1, 92)
-                    .TabIndex = 2
-                End With
-            End Sub
-
-            Private Sub SetPropertys_Panel_Title()
-                With PanelTitle
-                    .BackColor = TitleBarColor
-                    .Controls.Add(LabelClose)
-                    .Controls.Add(LabelTitle)
-                    .Location = New Point(0, -1)
-                    .Name = "PanelTitle"
-                    .Size = New Size(505, 29)
-                    .TabIndex = 0
-                End With
-            End Sub
-
-            Private Sub SetPropertys_Pb_Image()
-                With PictureBoxImage
-                    .BackColor = Color.Transparent
-                    .Location = New Point(12, 36)
-                    .Name = "PicBoxImage"
-                    .Size = New Size(92, 92)
-                    .TabIndex = 1
-                    .TabStop = False
-                    .Image = Image
-                    .SizeMode = PictureBoxSizeMode.StretchImage
-                    .Refresh()
-                End With
-            End Sub
-
-            Private Sub SetPropertys_Txt_Msg()
-                With RichTextBoxMessage
-                    .BackColor = TextFieldColor
-                    .ForeColor = FontColor
-                    .BorderStyle = BorderStyle.None
-                    .Location = New Point(133, 37)
-                    .Multiline = True
-                    .Name = "RiTxtMsg"
-                    .Size = New Size(361, 90)
-                    .TabIndex = 3
-                    .ReadOnly = True
-                    .Text = Message
-                    .SelectionProtected = True
-                    .Cursor = Cursors.Arrow
-                End With
-            End Sub
-
-            Protected Overrides Sub Finalize()
-                MyBase.Finalize()
-            End Sub
-
-            Private Sub InitializeComponent()
-                SuspendLayout()
-                '
-                'FormTemplate
-                '
-                ClientSize = New System.Drawing.Size(284, 261)
-                Name = "FormTemplate"
-                ResumeLayout(False)
-            End Sub
-
-        End Class
 
     End Class
 
