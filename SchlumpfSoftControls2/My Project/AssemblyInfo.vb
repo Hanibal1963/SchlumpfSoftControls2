7954e0ad1dfd7eaa8cfceb76b9d6fae532fc2fc8
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-22 14:54:24 +0100

    AssemblyInfo überarbeitet: Namespace und Kommentare
    - `Imports`-Anweisungen entfernt: `System.Reflection`,
      `System.Runtime.CompilerServices`, `System.Runtime.InteropServices`,
      `Microsoft.VisualBasic`.
    - Assembly-Attribute mit explizitem Namespace (`System.Reflection`,
      `System.Runtime.InteropServices`) neu hinzugefügt.
    - Reihenfolge der Attribute leicht angepasst, Funktionalität bleibt erhalten.
    - Attribute `<Assembly: ComVisible(False)>`, `<Assembly: AssemblyVersion>`,
      `<Assembly: AssemblyFileVersion>` beibehalten, aber mit explizitem Namespace.
    - Kommentare zu Assembly-Attributen vollständig entfernt.

--- SchlumpfSoftControls2/My Project/AssemblyInfo.vb
+++ SchlumpfSoftControls2/My Project/AssemblyInfo.vb
@@ -3,38 +3,14 @@
 ' Copyright (c) 2025 by Andreas Sauer 
 ' *************************************************************************************************
 
-Imports System.Reflection
-Imports System.Runtime.CompilerServices
-Imports System.Runtime.InteropServices
-Imports Microsoft.VisualBasic
-
-' Allgemeine Informationen zu einer Assembly werden durch den folgenden Satz
-' von Attributen bestimmt. Ändern Sie diese Attributwerte, um die einer
-' Assembly zugeordneten Informationen zu modifizieren.
-<Assembly: AssemblyTitle("SchlumpfSoftControls2")>
-<Assembly: AssemblyDescription("Eine Sammlung verschiedener Controls.")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("Andreas Sauer")>
-<Assembly: AssemblyProduct("SchlumpfSoftControls2")>
-<Assembly: AssemblyCopyright("Copyright © Andreas Sauer 2025")>
-<Assembly: AssemblyTrademark("privat")>
-<Assembly: AssemblyCulture("")>
-
-' Wenn ComVisible auf False gesetzt ist, sind die Typen in dieser Assembly
-' für COM-Komponenten nicht sichtbar. Wenn auf einen Typ in dieser Assembly
-' von COM aus zugegriffen werden muss, setzen Sie für diesen Typ das Attribut
-' ComVisible auf True.
-<Assembly: ComVisible(False)>
-
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion 
-'      Buildnummer
-'      Revision
-'
-' Sie können alle Werte angeben oder die Build- und Revisionsnummern
-' durch Verwendung von '*' wie unten gezeigt automatisch generieren lassen:
-' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("3.2025.1116.0")>
-<Assembly: AssemblyFileVersion("3.2025.1116.0")>
+<Assembly: System.Reflection.AssemblyTitle("SchlumpfSoftControls2")>
+<Assembly: System.Reflection.AssemblyDescription("Eine Sammlung verschiedener Controls.")>
+<Assembly: System.Reflection.AssemblyConfiguration("")>
+<Assembly: System.Reflection.AssemblyCompany("Andreas Sauer")>
+<Assembly: System.Reflection.AssemblyProduct("SchlumpfSoftControls2")>
+<Assembly: System.Reflection.AssemblyCopyright("Copyright © Andreas Sauer 2025")>
+<Assembly: System.Reflection.AssemblyTrademark("privat")>
+<Assembly: System.Reflection.AssemblyCulture("")>
+<Assembly: System.Reflection.AssemblyVersion("3.2025.1116.0")>
+<Assembly: System.Reflection.AssemblyFileVersion("3.2025.1116.0")>
+<Assembly: System.Runtime.InteropServices.ComVisible(False)>
