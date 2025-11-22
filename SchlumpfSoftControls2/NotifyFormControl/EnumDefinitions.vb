5264fae3578eeae865d7eb6d85c3a60d6629a0da
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-22 13:54:21 +0100

    Enum `NotifyFormDesign` verschoben und Projekt bereinigt
    Die Enumeration `NotifyFormDesign` wurde aus der Datei
    **NotifyFormDesign.vb** entfernt und in die Datei **EnumDefinitions.vb**
    verschoben. Dabei wurde die Dokumentation für die Enumeration und deren
    Werte hinzugefügt.
    
    Die Datei **NotifyFormDesign.vb** wurde vollständig gelöscht, da sie
    nicht mehr benötigt wird.
    
    Die Projektdatei **SchlumpfSoftControls2.vbproj** wurde aktualisiert:
    - **EnumDefinitions.vb** wurde hinzugefügt.
    - **NotifyFormDesign.vb** und **NotifyFormStyle.vb** wurden entfernt.

--- SchlumpfSoftControls2/NotifyFormControl/EnumDefinitions.vb
+++ SchlumpfSoftControls2/NotifyFormControl/EnumDefinitions.vb
@@ -1,9 +1,7 @@
 ﻿' *************************************************************************************************
-' NotifyFormStyle.vb
+' EnumDefinitionsvb
 ' Copyright (c) 2025 by Andreas Sauer 
 ' *************************************************************************************************
-
-' TODO: Code noch überarbeiten
 
 Namespace NotifyFormControl
 
@@ -34,4 +32,27 @@ Namespace NotifyFormControl
 
     End Enum
 
+    ''' <summary>
+    ''' Auflistung der Designs
+    ''' </summary>
+    Public Enum NotifyFormDesign As Integer
+
+        ''' <summary>
+        ''' Helles Design
+        ''' </summary>
+        Bright = 0
+
+        ''' <summary>
+        ''' Farbiges Design
+        ''' </summary>
+        Colorful = 1
+
+        ''' <summary>
+        ''' Dunkles Design
+        ''' </summary>
+        Dark = 2
+
+    End Enum
+
 End Namespace
+
