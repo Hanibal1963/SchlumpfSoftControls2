ff621a535106802024c898a2b3f8adc9857db5c4
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-30 18:06:21 +0100

    Formular zentriert beim Start positionieren
    - `Me.StartPosition` wurde zur `Form1`-Klasse hinzugefügt.
    - Das Formular wird nun standardmäßig in der Bildschirmmitte geöffnet.

--- Demos/ColorProgressBarControlDemo/Form1.Designer.vb
+++ Demos/ColorProgressBarControlDemo/Form1.Designer.vb
@@ -135,6 +135,7 @@ Partial Class Form1
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "ColorProgressBat - Control - Demo"
         CType(Me.NumericUpDownProgressValue, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
