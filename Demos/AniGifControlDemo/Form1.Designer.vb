a3d7ee7c175cf88b6ea7a944193322fb809ee331
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-11-30 18:07:24 +0100

    Formular-Startposition zentriert hinzugefügt
    - Die Eigenschaft `Me.StartPosition` wurde zur `Form1`-Klasse
      hinzugefügt.
    - Das Formular wird nun beim Start in der Mitte des Bildschirms
      positioniert.

--- Demos/AniGifControlDemo/Form1.Designer.vb
+++ Demos/AniGifControlDemo/Form1.Designer.vb
@@ -255,6 +255,7 @@ Partial Class Form1
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "AniGif - Control - Demo"
         Me.TableLayoutPanel.ResumeLayout(False)
         Me.TableLayoutPanel.PerformLayout()
