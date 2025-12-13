db9328d5e98a8476dc0b6a89445aad1fdb00bde3
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-12-13 18:19:16 +0100

    Eventklassen in einer Datei zusammengefasst

--- SchlumpfSoftControls2/WizardControl/PagesEventArgs.vb
+++ SchlumpfSoftControls2/WizardControl/PagesEventArgs.vb
@@ -1,9 +1,11 @@
 ﻿' *************************************************************************************************
-' BeforeSwitchPagesEventArgs.vb
+' PagesEventArgs.vb
 ' Copyright (c) 2025 by Andreas Sauer 
 ' *************************************************************************************************
 
 ' TODO: Code noch überarbeiten
+
+Imports System
 
 Namespace WizardControl
 
@@ -31,6 +33,38 @@ Namespace WizardControl
         Friend Sub New(OldIndex As Integer, NewIndex As Integer)
 
             MyBase.New(OldIndex, NewIndex)
+
+        End Sub
+
+    End Class
+
+    ''' <summary>
+    ''' Enthält die Indexwerte der Seiten nachdem die Seiten gewechselt wurden.
+    ''' </summary>
+    Public Class AfterSwitchPagesEventArgs
+
+        Inherits EventArgs
+
+        Protected _NewIndex As Integer
+
+        ''' <summary>
+        ''' Index der alten Seite
+        ''' </summary>
+        Public ReadOnly Property OldIndex As Integer
+
+        ''' <summary>
+        ''' Index der neuen Seite
+        ''' </summary>
+        Public ReadOnly Property NewIndex As Integer
+            Get
+                Return Me._NewIndex
+            End Get
+        End Property
+
+        Friend Sub New(OldIndex As Integer, NewIndex As Integer)
+
+            Me.OldIndex = OldIndex
+            Me._NewIndex = NewIndex
 
         End Sub
 
